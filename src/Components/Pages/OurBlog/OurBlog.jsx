c17b55f1dc308254cd38670e5c3cd4a419da8949
MasudRana14 <iammasud34@gmail.com>
2023-10-09 23:38:29 +0600

    change some blog page details

--- src/Components/Pages/OurBlog/OurBlog.jsx
+++ src/Components/Pages/OurBlog/OurBlog.jsx
@@ -20,7 +20,7 @@ const OurBlog = () => {
                         <a href="#">
                             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Healthy Habits for Busy Professionals: Balancing Work and Wellness</h5>
                         </a>
-                        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">In today’s fast-paced world, juggling the demands of a demanding career with maintaining a healthy lifestyle can be a challenging task. For many busy professionals, work often takes precedence over personal well-being, leading to neglect of essential health habits..... </p>
+                        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">In todays fast-paced world, juggling the demands of a demanding career with maintaining a healthy lifestyle can be a challenging task. For many busy professionals, work often takes precedence over personal well-being, leading to neglect of essential health.... </p>
                         <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                         Continue Reading
 
@@ -37,7 +37,7 @@ const OurBlog = () => {
                         <a href="#">
                             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Could You Benefit From an MSN-MBA Dual Degree?</h5>
                         </a>
-                        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">The Master of Science in Nursing-Master of Business Administration (MSN-MBA) dual degree is getting more attention from registered nurses (RNs). This degree program combines advanced nursing education with executive leader and business administration skills in preparation for high-level executive positions within the health care field..... </p>
+                        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">The Master of Science in Nursing-Master of Business Administration (MSN-MBA) dual degree is getting more attention from registered nurses (RNs). This degree program combines advanced nursing education with executive leader and business administration skills in preparation for high-level executive positions within the health care..... </p>
                         <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                         Continue Reading
 
@@ -55,7 +55,7 @@ const OurBlog = () => {
                             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">The Importance of Community Health Nursing</h5>
                         </a>
                         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Community health nurses are a vital link in the health and welfare of neighborhoods. These registered nurses focus on improving the lives of diverse communities of infants, children, adolescents, and adults through education, prevention, and treatment.
-                        “They assist with equalizing health care in diverse populations, so that care is more equitable, affordable, and well-coordinated,” says Sheila Rucki, faculty member in the Purdue Global School of Nursing.... </p>
+                        “They assist with equalizing health care in diverse populations, so that care is more equitable, affordable, and well-coordinated,” says Sheila, faculty member in the Purdue Global School of Nursing.... </p>
                         <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                         Continue Reading
 
@@ -106,7 +106,7 @@ const OurBlog = () => {
                         <a href="#">
                             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Practicing Meditation for Mental Health and Wellbeing</h5>
                         </a>
-                        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">What comes to mind when you imagine practicing meditation? Is it sitting cross-legged in silence as the outside world races by? Is it clearing your mind of all thoughts in the hope of attaining enlightenment? The truth is that meditation practices come in many different forms and can provide various benefits for anyone seeking inner calm and self-discovery... </p>
+                        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">What comes to mind when you imagine practicing meditation? Is it sitting cross-legged in silence as the outside world races by? Is it clearing your mind of all thoughts in the hope of attaining enlightenment? The truth is that meditation practices come in many different forms and can provide various benefits for anyone seeking inner... </p>
                         <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                         Continue Reading
 
